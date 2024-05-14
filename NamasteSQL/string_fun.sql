281627ce91d44b15aece5180e1e244b20b6de2eb
Sourav Ganesha <50478353+SouravGanesh@users.noreply.github.com>
2024-05-14 15:05:23 -0400

    Update string_fun.sql

--- NamasteSQL/string_fun.sql
+++ NamasteSQL/string_fun.sql
@@ -22,3 +22,9 @@ select order_id,customer_name
 ,concat(order_id,'-',customer_name)
 ,order_id+'-'+customer_name 
 from orders;
+
+--2- write a query to print first name and last name of a customer using orders table(everything after first space can be considered as last name)
+--customer_name, first_name,last_name
+select customer_name , trim(SUBSTRING(customer_name,1,CHARINDEX(' ',customer_name))) as first_name
+, SUBSTRING(customer_name,CHARINDEX(' ',customer_name)+1,len(customer_name)-CHARINDEX(' ',customer_name)+1) as second_name
+from orders
